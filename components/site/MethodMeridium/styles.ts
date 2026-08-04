24058e632bb464c93386858d3bf78d21fb76e58c
Meridiumseg <douglas.depaula.sst@gmail.com>
2026-08-04 14:03:26 -0300

    Add styles for MethodMeridium component

---
+++ components/site/MethodMeridium/styles.ts
@@ -0,0 +1,47 @@
+export const section = `
+relative
+overflow-hidden
+bg-zinc-950
+py-32
+`;
+
+export const title = `
+text-6xl
+font-black
+text-white
+`;
+
+export const subtitle = `
+text-lg
+leading-9
+text-zinc-400
+`;
+
+export const badge = `
+inline-flex
+rounded-full
+border
+border-green-700/30
+bg-green-900/20
+px-5
+py-2
+text-xs
+uppercase
+tracking-[0.35em]
+font-semibold
+text-green-400
+`;
+
+export const timeline = `
+absolute
+left-1/2
+top-0
+hidden
+h-full
+w-px
+-lg:block
+bg-gradient-to-b
+from-green-700
+via-green-500
+to-transparent
+`;
