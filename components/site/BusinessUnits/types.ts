5c240f8bc823f4366151f930dcaea8a1f3bb632f
Meridiumseg <douglas.depaula.sst@gmail.com>
2026-08-04 13:52:29 -0300

    Add BusinessUnit interface definition

---
+++ components/site/BusinessUnits/types.ts
@@ -0,0 +1,11 @@
+export interface BusinessUnit {
+  id: string;
+  title: string;
+  subtitle: string;
+  description: string;
+  href: string;
+  image: string;
+  icon: string;
+  color: string;
+  services: string[];
+}
